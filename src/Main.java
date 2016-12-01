bdb20c666caf8a2bea67ca83a53097a3c868c726
aaronpries <ae.pries@gmail.com>
2016-12-01 16:32:38 -0700

    Weird Remove Error - Panic Pushing

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,11 @@ public class Main {
         //Card mycard = new Card("4s");
         Card h = new Card(new String[] {"3c", "4s", "5d", "6h", "7h"});
         h.PrintHand();
+//        h.addCard("8d");
+//        h.addCard("3d");
+//        h.PrintHand();
+        h.removeCard(1);
+        h.PrintHand();
 
     }
 
