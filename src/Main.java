6657579372c022631a97b8e2cb01ef04141d5550
aaronpries <ae.pries@gmail.com>
2016-12-03 15:45:31 -0700

    Hand stats- Not working
    Not working, but I want to back up the code I have

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,21 @@ public class Main {
 
     public static void main(String args[]){
 
-        Card h = new Card(new String[] {"3c", "4s", "5d", "6h", "7h"});
+        Card h = new Card(new String[] {"3c", "3s", "Ad", "6h", "7h", "As"});
         h.PrintHand();
-        h.addCard("8d");
-        h.addCard("3d");
-        h.removeCard(2);
-        h.PrintHand();
+//        h.addCard("8d");
+//        h.addCard("As");
+        //h.hiCard();
+//        h.addCard("3d");
+//        h.removeCard(2);
+//        h.PrintHand();
 //        h.addCard("9s");
 //        h.addCard("5h");
 //        h.addCard("As");
 //        h.addCard("5s");
-       h.removeCard("3c");
-        h.PrintHand();
+//       h.removeCard("3c");
+//        h.PrintHand();
+
     }
 
 
